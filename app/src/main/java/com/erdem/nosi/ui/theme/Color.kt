893442452b87ae6c9958df3d94f7a46ff3aa6cfd
ErdemKaraca53 <erdemkaraca5328@gmail.com>
2025-11-26 00:29:02 +0300

    yeni renkler

--- app/src/main/java/com/erdem/nosi/ui/theme/Color.kt
+++ app/src/main/java/com/erdem/nosi/ui/theme/Color.kt
@@ -6,8 +6,20 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val Black = Color(0xFF000000)
+
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
 val TopBarColor = Color(0xFF211C12)
+val MainBackgroundrColor = Color(0xFF211C12)
 val White = Color(0xFFFFFFFF)
+val MainYellowText = Color(0xFFC9BA91)
+val MainStartButton = Color(0xFFF5C754)
+
+val IconBoxBackground = Color(0xFF473D24)
+val RowTextColor = Color(0xFFC9BA91)
+
+val BottomAppBarBackground = Color(0xFF332B1A)
+val BottomAppBarContent = Color(0xFFC9BA91)
