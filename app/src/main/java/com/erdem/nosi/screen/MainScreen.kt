830b21d2e98fa6c31f56aabbbfcb3c14cb35c634
ErdemKaraca53 <erdemkaraca5328@gmail.com>
2025-11-27 01:38:41 +0300

    gereksiz importlar silindi

--- app/src/main/java/com/erdem/nosi/screen/MainScreen.kt
+++ app/src/main/java/com/erdem/nosi/screen/MainScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
