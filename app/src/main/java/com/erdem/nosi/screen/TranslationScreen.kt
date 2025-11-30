bcf96326fcdcc902f5b598e61c553e8c10d0d6a0
ErdemKaraca53 <erdemkaraca5328@gmail.com>
2025-11-30 15:36:49 +0300

    Çeviri kutusu tasarımı ilerletildi. Alternatif cümleler için gerekli kutu surface tasarımı yapıldı.

--- app/src/main/java/com/erdem/nosi/screen/TranslationScreen.kt
+++ app/src/main/java/com/erdem/nosi/screen/TranslationScreen.kt
@@ -3,20 +3,18 @@ package com.erdem.nosi.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -24,16 +22,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,10 +42,16 @@ import androidx.compose.ui.unit.sp
 import com.erdem.nosi.R
 import com.erdem.nosi.ui.theme.AiTutorTextColor
 import com.erdem.nosi.ui.theme.MainBackgroundrColor
-import com.erdem.nosi.ui.theme.PurpleGrey40
-import com.erdem.nosi.ui.theme.SelectedTranslete
+import com.erdem.nosi.ui.theme.SelectedTransleteContainer
+import com.erdem.nosi.ui.theme.SelectedTransleteText
 import com.erdem.nosi.ui.theme.TranslationContainerBackground
+import com.erdem.nosi.ui.theme.UnSelectedTransleteContainer
+import com.erdem.nosi.ui.theme.UnSelectedTransleteText
 import com.erdem.nosi.ui.theme.White
+
+val LexendFontFamily = FontFamily(
+    Font(R.font.lexend)
+)
 
 @Composable
 fun TranslationScaffol() {
@@ -111,14 +116,15 @@ fun AiInfo() {
                 text = stringResource(R.string.AiTextTitle),
                 color = AiTutorTextColor,
                 fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                fontFamily = LexendFontFamily
             )
             Surface(
                 modifier = Modifier
                     .padding(start = 12.dp, top = 8.dp, bottom = 12.dp, end = 20.dp)
                     .wrapContentHeight()
                     .fillMaxWidth()/*Padding for surface*/,
-                color = TranslationContainerBackground,
+                color = Color(0xFF233C48),
                 shape = RoundedCornerShape(12.dp)
             ) {
                 Text(
@@ -128,7 +134,8 @@ fun AiInfo() {
                     fontSize = 16.sp,
                     textAlign = TextAlign.Start,
                     lineHeight = TextUnit.Unspecified,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Normal,
+                    fontFamily = LexendFontFamily
                 )
             }
         }
@@ -138,23 +145,23 @@ fun AiInfo() {
 }
 
 @Composable
-fun TextSurface(modifier: Modifier) {
+fun TextSurface(color: Color, textColor: Color, text: String) {
     Surface(
         modifier = Modifier
-            .padding(start = 12.dp, top = 8.dp, bottom = 12.dp, end = 20.dp)
+            .padding(start = 12.dp, bottom = 12.dp, end = 20.dp)
             .wrapContentHeight()
             .fillMaxWidth()/*Padding for surface*/,
-        color = SelectedTranslete,
+        color = color,
         shape = RoundedCornerShape(12.dp)
     ) {
         Text(
             modifier = Modifier.padding(16.dp),
             text = "Which color do you like most",
-            color = White,
-            fontSize = 16.sp,
+            color = textColor,
+            fontSize = 17.sp,
             textAlign = TextAlign.Start,
             lineHeight = TextUnit.Unspecified,
-            fontWeight = FontWeight.Bold
+            fontFamily = LexendFontFamily
         )
     }
 }
@@ -180,16 +187,18 @@ fun TransletedSentence() {
                 fontSize = 16.sp,
                 textAlign = TextAlign.Start,
                 lineHeight = TextUnit.Unspecified,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = LexendFontFamily
             )
 
             Text(
                 modifier = Modifier.padding(start = 16.dp, bottom = 16.dp, end = 16.dp),
                 text = "What is your favorite color",
                 color = White,
-                fontSize = 20.sp,
+                fontSize = 18.sp,
                 textAlign = TextAlign.Start,
                 lineHeight = TextUnit.Unspecified,
+                fontFamily = LexendFontFamily
             )
             HorizontalDivider(
                 Modifier.padding(start = 16.dp, end = 16.dp),
@@ -199,17 +208,40 @@ fun TransletedSentence() {
             Text(
                 modifier = Modifier.padding(16.dp),
                 text = "Alternative english Sentences",
-                color = White,
-                fontSize = 20.sp,
+                color = Color(0xFF9CA3AF),
+                fontSize = 16.sp,
                 textAlign = TextAlign.Start,
                 lineHeight = TextUnit.Unspecified,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = LexendFontFamily
             )
-            TextSurface(modifier = Modifier)
-
+            //Api'den gelen yanıt yazdırılacak
+            TextSurface(
+                color = SelectedTransleteContainer,
+                textColor =  SelectedTransleteText,
+                text = "")
+            TextSurface(
+                color = UnSelectedTransleteContainer,
+                textColor = UnSelectedTransleteText,
+                text = ""
+            )
+            HorizontalDivider(
+                Modifier.padding(start = 16.dp, end = 16.dp, top = 12.dp),
+                DividerDefaults.Thickness,
+                DividerDefaults.color
+            )
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = "Breakdown for \"Which color do you like the most?\"",
+                color = Color(0xFF9CA3AF),
+                fontSize = 15.sp,
+                textAlign = TextAlign.Start,
+                lineHeight = TextUnit.Unspecified,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = LexendFontFamily
+            )
         }
     }
-
-
 }
 
 
